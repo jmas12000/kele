81dacb3f650c98749e74dc55f8e39959777dfacc
Jeff Moore <jmas1_2000@hotmail.com>
2017-03-20 05:35:17 +0000

    Mentor Availability: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,6 +2,7 @@ require "kele/version"
 require "httparty"
 require "json"
 require "kele/errors"
+require "awesome_print"
 
 class Kele
   include HTTParty
@@ -15,5 +16,10 @@ class Kele
   def get_me
     response = self.class.get("/users/me", headers: { "authorization" => @auth_token})
     @current_user = JSON.parse(response.body) 
+  end
+  
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token})
+    @mentor_times = JSON.parse(response.body)
   end
 end
